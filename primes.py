b01393e2e5e1780ec3e296bd7b4240398f81d0f3
Poorna Venkata Sai <ad.compugit@gmail.com>
2022-11-06 22:22:28 +0530

    initial release

---
+++ primes.py
@@ -0,0 +1,47 @@
+class Primes:
+
+    def __init__(self, n=2, m=20):
+        self.n = n-1
+        self.range = n,m
+        self._list = []
+        while n<=m:
+            if (x:=next(self._gen()))<=m: self._list.append(x)
+            n += 1
+
+        
+
+    def __repr__(self) -> str:
+        return f'Primes{self.range}  object at id<{hex(id(self))}>'
+
+    def __iter__(self):
+        return iter(self._list)
+    
+    def __len__(self):
+        return len(self._list)
+
+    def _gen(self):
+        while True:
+            self.n+=1
+            if self.isprime(self.n): yield self.n
+
+    def isprime(self, n:int ) -> bool:
+        if (n <= 1): return False
+        elif (n == 2): return True
+        elif (n % 2 == 0): return False
+        else:
+            i = 3
+            while i <= n**(1/2):
+                if n % i == 0: return False
+                i += 2
+            return True
+    
+'''
+p = Primes(10,30)
+print(p)
+print(list(p))
+print(len(p))
+print(type(p))
+for i in p:
+    print(i)
+#print(Primes().isprime(7)
+'''
