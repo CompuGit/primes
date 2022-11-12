2ac9e1b7e9aaa99fcb7ee90953ff5347132d2d29
Poorna Venkata Sai <ad.compugit@gmail.com>
2022-11-12 12:41:58 +0530

    Update primes.py

--- primes.py
+++ primes.py
@@ -35,3 +35,34 @@ class Primes:
                 i += 2
             return True
         
+'''
+class Primes:
+
+    def __init__(self, n=2, m=20):
+        self.n = n-1
+        self.m = m
+
+    def __repr__(self) -> str:
+        return f'Primes{self.n+1, self.m}  object at id<{hex(id(self))}>'
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.n>self.m:
+            raise StopIteration
+        #if self.isprime(self.n):
+        self.n+=1
+        return self.n if self.isprime(self.n) else self.__next__()
+            
+    def isprime(self, n:int ) -> bool:
+        if (n <= 1): return False
+        elif (n == 2): return True
+        elif (n % 2 == 0): return False
+        else:
+            i = 3
+            while i <= n**(1/2):
+                if n % i == 0: return False
+                i += 2
+            return True
+'''
